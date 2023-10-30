cfc4c260cc3d112dbe298ffa5f3dcb107a21202d
Simeon Lees <simeon.k.lees@gmail.com>
2023-10-30 09:52:02 +0000

    Factor out usePieceUpdates

--- components/JigsawCanvas.tsx
+++ components/JigsawCanvas.tsx
@@ -1,22 +1,14 @@
 import styled from 'styled-components';
-import { DragPieceInfo } from '../types';
 import Piece from './Piece';
-import { useEffect, useRef, useState } from 'react';
-import { getMousePosWithinElement } from '../utils/dom';
+import { useRef } from 'react';
 import { keyboardShortcuts } from '../constants/keyboardShortcuts';
-import { PIECE_ROTATION_INTERVAL } from '../constants/uiConfig';
 import {
   CANVAS_WIDTH,
   CANVAS_HEIGHT,
   JIGSAW_CONFIG,
 } from '../constants/jigsawConfig';
 import useGame from '../hooks/useGame';
-import {
-  pickUpPiece,
-  releasePiece,
-  rotatePiece,
-  setPiecePos,
-} from '../lib/actions';
+import usePieceUpdates from '../hooks/usePieceUpdates';
 import useUser from '../hooks/useUser';
 
 const CanvasWrapper = styled.div`
@@ -27,8 +19,19 @@ const CanvasWrapper = styled.div`
 `;
 
 export default function JigsawCanvas() {
-  const { jigsaw: jigsawState, gameKey } = useGame();
+  const { jigsaw: jigsawState } = useGame();
   const { user } = useUser();
+
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  const {
+    dragPieceInfo,
+    setRotatingDirection,
+    onCancelDrag,
+    onDrag,
+    onMouseDown,
+  } = usePieceUpdates({ canvasRef });
+
   if (!jigsawState) {
     // TODO maybe enforce this through context?
     throw new Error('jigsawState not defined but it should be!!!');
@@ -38,56 +41,7 @@ export default function JigsawCanvas() {
     throw new Error('TODO this should never happen');
   }
 
-  const canvasRef = useRef<HTMLDivElement>(null);
-
-  const [dragPieceInfo, setDragPieceInfo] = useState<DragPieceInfo | null>(
-    null,
-  );
-  const [rotatingDirection, setRotatingDirection] = useState<
-    'clockwise' | 'anticlockwise' | null
-  >(null);
-
   const { draggingPieceKey } = dragPieceInfo || {};
-
-  const updatePieceRotationInterval = useRef<NodeJS.Timer | null>(null);
-
-  const clearRotationInterval = () => {
-    if (typeof updatePieceRotationInterval.current === 'number') {
-      clearInterval(updatePieceRotationInterval.current);
-    }
-  };
-
-  const maybeCancelDrag = () => {
-    if (!dragPieceInfo) {
-      return;
-    }
-    releasePiece({
-      gameKey,
-      pieceKey: dragPieceInfo.draggingPieceKey,
-      uid: user.uid,
-    });
-    setDragPieceInfo(null);
-    setRotatingDirection(null);
-    clearRotationInterval();
-  };
-
-  useEffect(() => {
-    if (rotatingDirection && draggingPieceKey) {
-      const doRotation = () =>
-        rotatePiece({
-          gameKey,
-          pieceKey: draggingPieceKey,
-          direction: rotatingDirection,
-        });
-
-      doRotation();
-      updatePieceRotationInterval.current = setInterval(() => {
-        doRotation();
-      }, PIECE_ROTATION_INTERVAL);
-
-      return clearRotationInterval;
-    }
-  }, [rotatingDirection, draggingPieceKey, gameKey]);
 
   return (
     <CanvasWrapper
@@ -108,26 +62,11 @@ export default function JigsawCanvas() {
       onKeyUp={() => {
         setRotatingDirection(null);
       }}
-      onMouseUp={() => maybeCancelDrag()}
-      onMouseLeave={() => maybeCancelDrag()}
+      onMouseUp={() => onCancelDrag()}
+      onMouseLeave={() => onCancelDrag()}
       onMouseMove={(e) => {
-        if (dragPieceInfo && canvasRef.current && draggingPieceKey) {
-          const { top, left } = getMousePosWithinElement(e, canvasRef.current);
-          const pieceTop = top - dragPieceInfo.initialPieceMouseOffsetY;
-          const pieceLeft = left - dragPieceInfo.initialPieceMouseOffsetX;
-
-          // // TODO do we want to store "uncomitted" piece state locally?
-          // setPieceState(draggingPieceKey, {
-          //   top: pieceTop,
-          //   left: pieceLeft,
-          // });
-
-          setPiecePos({
-            gameKey,
-            pieceKey: draggingPieceKey,
-            left: pieceLeft,
-            top: pieceTop,
-          });
+        if (dragPieceInfo) {
+          onDrag(e);
         }
       }}
     >
@@ -139,29 +78,7 @@ export default function JigsawCanvas() {
             pieceState={pieceState}
             jigsawConfig={JIGSAW_CONFIG}
             onMouseDown={(e) => {
-              if (canvasRef.current) {
-                pickUpPiece({
-                  gameKey,
-                  pieceKey,
-                  uid: user.uid,
-                }).then((transactionResult) => {
-                  if (transactionResult.committed && canvasRef.current) {
-                    // only here do we want to send piece updates
-                    const { top, left } = getMousePosWithinElement(
-                      e,
-                      canvasRef.current,
-                    );
-                    const initialPieceMouseOffsetX = left - pieceState.left;
-                    const initialPieceMouseOffsetY = top - pieceState.top;
-
-                    setDragPieceInfo({
-                      draggingPieceKey: pieceKey,
-                      initialPieceMouseOffsetX,
-                      initialPieceMouseOffsetY,
-                    });
-                  }
-                });
-              }
+              onMouseDown(e, pieceKey, pieceState);
             }}
             isDragging={draggingPieceKey === pieceKey}
           />
