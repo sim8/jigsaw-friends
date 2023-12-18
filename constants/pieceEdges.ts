17b0f629ea1f92bdc4350b7c9543b8efc237e910
Simeon Lees <simeon.k.lees@gmail.com>
2023-12-18 21:34:58 +0000

    Add another sample edge

--- constants/pieceEdges.ts
+++ constants/pieceEdges.ts
@@ -10,7 +10,7 @@ import { SvgPath } from '../types';
 
 export const FLAT_EDGE: SvgPath = [['L', [100, 0]]];
 
-const REGULAR_TAB_EDGE: SvgPath = [
+const POINTED_TAB_EDGE: SvgPath = [
   ['Q', [30, 10], [37, 7]],
   ['T', [40, -5]],
   ['Q', [38, -7], [45, -10]],
@@ -19,4 +19,14 @@ const REGULAR_TAB_EDGE: SvgPath = [
   ['T', [100, 0]],
 ];
 
-export const NON_FLAT_EDGES = [REGULAR_TAB_EDGE];
+const FAT_TAB_EDGE: SvgPath = [
+  ['Q', [30, 10], [37, 7]],
+  ['T', [38, -5]],
+  ['Q', [38, -10], [43, -12]],
+  ['T', [51, -11]],
+  ['T', [51, -4]],
+  ['T', [50, 4]],
+  ['T', [100, 0]],
+];
+
+export const NON_FLAT_EDGES = [POINTED_TAB_EDGE, FAT_TAB_EDGE];
